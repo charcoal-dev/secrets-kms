51778482a9699601d056699784c0701c128afe32
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-11 18:26:45 +0500

    Add `SecretsNamespace` class for managing secrets securely.

---
+++ src/SecretsNamespace.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Part of the "charcoal-dev/secrets-kms" package.
+ * @link https://github.com/charcoal-dev/secrets-kms
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Security\Secrets;
+
+use Charcoal\Contracts\Buffers\Sensitive\SensitiveKeyBufferInterface;
+use Charcoal\Contracts\Security\Secrets\SecretNamespaceInterface;
+use Charcoal\Contracts\Security\Secrets\SecretsKms;
+use Charcoal\Contracts\Security\Secrets\SecretStorageInterface;
+use Charcoal\Security\Secrets\Types\SecretKey32;
+
+/**
+ * A namespace for managing secrets.
+ */
+final readonly class SecretsNamespace implements SecretNamespaceInterface
+{
+    public function __construct(
+        private SecretStorageInterface $storage,
+        private string                 $refId,
+    )
+    {
+        if (!$this->refId || !preg_match(SecretsKms::NAMESPACE_REGEXP, $this->refId) || strlen($this->refId) > 163) {
+            throw new \InvalidArgumentException("Invalid namespace path");
+        }
+    }
+
+    /**
+     * Loads a secret from the namespace.
+     */
+    public function load(string $id, int $version): SecretKey32
+    {
+        return $this->storage->load($id, $version, $this);
+    }
+
+    /**
+     * Stores a secret in the namespace.
+     */
+    public function store(string $id, int $version, SensitiveKeyBufferInterface $key): void
+    {
+        $this->storage->store($this, $id, $version, $key);
+    }
+
+    /**
+     * Deletes a secret from the namespace.
+     */
+    public function delete(string $id, int $version): void
+    {
+        $this->storage->delete($id, $version, $this);
+    }
+
+    /**
+     * Checks if the secret exists in the namespace.
+     */
+    public function has(string $id, int $version): bool
+    {
+        return $this->storage->has($id, $version, $this);
+    }
+
+    /**
+     * Returns a list of all secrets in the namespace.
+     * @return string[]
+     */
+    public function list(): array
+    {
+        return $this->storage->list($this);
+    }
+
+    /**
+     * @return string
+     */
+    public function refId(): string
+    {
+        return $this->refId;
+    }
+}
