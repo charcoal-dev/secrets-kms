c56db0914db509bc6785afb7eb127ef8c1b856b9
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-11 19:30:14 +0500

    Add `KeySize` enum with fixed byte lengths and FQCN resolution

---
+++ src/Enums/KeySize.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Part of the "charcoal-dev/secrets-kms" package.
+ * @link https://github.com/charcoal-dev/secrets-kms
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Security\Secrets\Enums;
+
+use Charcoal\Contracts\Security\Secrets\SecretKeyInterface;
+use Charcoal\Security\Secrets\Types\SecretKey16;
+use Charcoal\Security\Secrets\Types\SecretKey20;
+use Charcoal\Security\Secrets\Types\SecretKey24;
+use Charcoal\Security\Secrets\Types\SecretKey32;
+
+/**
+ * Key Types
+ */
+enum KeySize: int
+{
+    case Bytes16 = 16;
+    case Bytes20 = 20;
+    case Bytes24 = 24;
+    case Bytes32 = 32;
+    case Bytes40 = 40;
+    case Bytes64 = 64;
+
+    /**
+     * Returns the type of the secret key.
+     * @return class-string<SecretKeyInterface>
+     */
+    public function getTypeFqcn(): string
+    {
+        return match ($this) {
+            self::Bytes16 => SecretKey16::class,
+            self::Bytes20 => SecretKey20::class,
+            self::Bytes24 => SecretKey24::class,
+            self::Bytes32 => SecretKey32::class,
+            default => throw new \RuntimeException("No secret buffer available for: " . $this->name),
+        };
+    }
+}
