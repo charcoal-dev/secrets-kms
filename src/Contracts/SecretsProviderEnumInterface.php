4a8b9ba5a762d230430441a18f7def40b01adaf6
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-11 19:28:41 +0500

    Add `getKeySize` method to `SecretsProviderEnumInterface` and include `KeySize` usage

--- src/Contracts/SecretsProviderEnumInterface.php
+++ src/Contracts/SecretsProviderEnumInterface.php
@@ -8,6 +8,8 @@ declare(strict_types=1);
 
 namespace Charcoal\Security\Secrets\Contracts;
 
+use Charcoal\Security\Secrets\Enums\KeySize;
+
 /**
  * Interface SecretsProviderEnumInterface
  * Defines a contract for enums that provide secrets and their path resolution.
@@ -17,4 +19,6 @@ interface SecretsProviderEnumInterface extends \UnitEnum
     public function getId(): string;
 
     public function resolvePath(): string;
+
+    public function getKeySize(): KeySize;
 }
